9a60d3869c32dc1f70a29d049c83e9b8ed743dc5
Martynas Gumuliauskas <Mourcius@gmail.com>
2022-12-11 20:54:16 +0200

    Functions and declared types, tsconfig options.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
-let numbers: number[] = [];
-numbers[0] = 1;
-let user: [number, string] = [1, 'Martynas'];
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear  < 2022) return income * 1.2;
 
+  return income * 1.3;
+}
 
-//Enum
-// PascalCase
-const enum Size { Small = 1, Medium, Large }
-let mySize: Size = Size.Medium;
-console.log(mySize)
-
+calculateTax(10_000)
